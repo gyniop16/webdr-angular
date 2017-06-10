70a32a724338beff73276c8037206135e042f580
Alejandro Mejia <alejandro.mmurillo@hotmail.com>
2017-06-09 19:25:57 -0500

    Adding mock service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataMockService } from './data-mock-service';
 
 import { AppComponent } from './app.component';
+
+import { environment } from '../environments';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    !environment.production ? InMemoryWebApiModule.forRoot(DataMockService) : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
